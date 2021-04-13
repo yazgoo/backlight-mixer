aaa3910de2208903b95d8e6c7662e10727fd12be
yazgoo <yazgoo@gmail.com>
2021-04-13 22:00:03 +0200

    🤞 first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # backlight-mixer
+
 like alsamixer but for your screen backlight
+
+# usage
+
+To view current value:
+
+`cargo build && sudo ./target/debug/backlight-mixer`
+
+To change current value (in percent):
+
+`cargo build && sudo ./target/debug/backlight-mixer 50`
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::fs;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::env;
+
+struct Backlight {
+    path: String, 
+}
+
+type Percent = u8;
+
+const BACKLIGHT_DIRECTORY: &str = "/sys/class/backlight";
+
+fn list_backlights() -> Result<Vec<Backlight>, Box<dyn Error>> {
+    let paths = fs::read_dir(BACKLIGHT_DIRECTORY)?;
+    Ok(paths.flat_map(|path| {
+        path.map(|x| {
+            let file_name = x.file_name();
+            file_name.into_string().map ( |n| Backlight { path: format!("{}/{}", &BACKLIGHT_DIRECTORY, n) } )
+        })}).flatten().collect())
+}
+
+impl Backlight {
+
+    fn read_file(&self, file: &str) -> Result<u32, Box<dyn Error>> {
+        let mut x = fs::read_to_string(format!("{}/{}", self.path, file))?;
+        x.truncate(x.len() - 1);
+        Ok(x.parse::<u32>()?)
+    }
+
+    fn get_maximum(&self) -> Result<u32, Box<dyn Error>> {
+        self.read_file("max_brightness")
+    }
+
+    fn get_brightness(&self) -> Result<Percent, Box<dyn Error>> {
+        let max = self.get_maximum()?;
+        let current = self.read_file("brightness")?;
+        Ok((current * 100 / max) as Percent)
+    }
+
+    fn set_brightness(&self, percent: Percent) -> Result<(), Box<dyn Error>> {
+        let max = self.get_maximum()?;
+        let value = percent as u32 * max / 100;
+        let path = format!("{}/brightness", self.path);
+        let mut ofile = File::create(&path)?;
+        ofile.write_all(format!("{}\n", value).as_bytes())?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    for b in list_backlights()?.iter() {
+        println!("{}", b.path);
+        println!("{}", b.get_brightness()?);
+        if args.len() > 1 {
+            b.set_brightness(args[1].parse::<Percent>()?)?;
+        }
+    }
+    Ok(())
+}
